9e1bd29f3e3985d94c5e615f039d0cf610d2631b
Mirko <mkcampo2000@gmail.com>
2022-01-05 22:43:33 +0100

    fixed responsiveness

--- src/components/ParkingDiv.js
+++ src/components/ParkingDiv.js
@@ -13,8 +13,8 @@ class ParkingDiv extends React.Component {
       const surveillance_color = this.props.surveillance_color;
       return (
         <div style={{background:'white', margin:'5px'}}> {/* TODO: remove */}
-        <ListItem>
-          <List style={{background:'lightgreen'}}>
+        <ListItem style={{background:"pink"}}>
+          <List id="listResponsive" style={{background:'lightgreen'}}>
             <ListItem disablePadding className='mildSpacing updown' style={{background:'red'}}>
               <LocationOnSharpIcon style={{background:'yellow'}}/>
               <p className='mildSpacing sides'>{parking.address}</p>
