81c6b1b70f0095c2e6dce07bc96009b96f0468bb
Mirko <mkcampo2000@gmail.com>
2022-01-08 21:02:01 +0100

    tweaked styling a bit

--- src/components/TopHeaders.js
+++ src/components/TopHeaders.js
@@ -4,13 +4,11 @@ class TopHeaders extends React.Component {
     render() {
       const problemDescription = 'Solving bicycle theft with a people-first approach';
       return (
-        <div id="topHeader">
-          <div className="row">
-            <div className="column left mildSpacing">
-              <h1 className="headers" id="titleHeading">spazi-per-pedalare</h1>
+          <div id="topHeader" className="row" /* style={{background:'red'}} */>
+            <div className="column left mildSpacing" style={{width:'100%'/* , background:'orange' */}}>
+              <h1 className="headers" id="titleHeading" /* style={{background:"yellow"}} */>hype-hype-hype</h1>
             </div> 
           </div>
-        </div>
       )
     }
   }
