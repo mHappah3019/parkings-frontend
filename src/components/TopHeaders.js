f7261f9f9b84239d514a378297413ba8ea1e3b62
Mirko Camporesi <mkcampo2000@gmail.com>
2022-01-17 22:23:19 +0100

    fixed x-scroll definetely

--- src/components/TopHeaders.js
+++ src/components/TopHeaders.js
@@ -5,7 +5,7 @@ class TopHeaders extends React.Component {
       const problemDescription = 'Solving bicycle theft with a people-first approach';
       return (
           <div id="topHeader" className="row" /* style={{background:'red'}} */>
-            <div className="column left mildSpacing" style={{width:'100%'/* , background:'orange' */}}>
+            <div className="column left" style={{width:'100%'/* , background:'orange' */}}>
               <h1 className="headers" id="titleHeading" /* style={{background:"yellow"}} */>hype-hype-hype</h1>
             </div> 
           </div>
