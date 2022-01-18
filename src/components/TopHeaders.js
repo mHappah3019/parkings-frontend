de5dea862a043bcc98a58f5d8594c61c24a35bc3
Mirko Camporesi <mkcampo2000@gmail.com>
2022-01-18 07:27:11 +0100

    really noice

--- src/components/TopHeaders.js
+++ src/components/TopHeaders.js
@@ -5,8 +5,8 @@ class TopHeaders extends React.Component {
       const problemDescription = 'Solving bicycle theft with a people-first approach';
       return (
           <div id="topHeader" className="row" /* style={{background:'red'}} */>
-            <div className="column left" style={{width:'100%'/* , background:'orange' */}}>
-              <h1 className="headers" id="titleHeading" /* style={{background:"yellow"}} */>hype-hype-hype</h1>
+            <div className="column left" style={{/* , background:'orange' */}}>
+              <h1 className="headers" id="titleHeading" /* style={{background:"yellow"}} */>Open Custody</h1>
             </div> 
           </div>
       )
